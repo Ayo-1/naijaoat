04bcf5725ed77db3f9f9e16006c2879323290ed2
Ayomide Oloyede <52506241+Ayo-1@users.noreply.github.com>
2021-12-20 13:24:43 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-    }
-     if($this->app->environment('production')) {
+        
+        if($this->app->environment('production')) {
       \URL::forceScheme('https');
         }
+    }
+ 
 }
